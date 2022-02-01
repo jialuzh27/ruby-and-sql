fa7f7f9045dab8e7ec8497f580bcb66e1993c70f
jialuzh27 <jialu.zhou@kellogg.northwestern.edu>
2022-02-01 14:58:25 +0000

    update

--- code-along/0-classes.rb
+++ code-along/0-classes.rb
@@ -3,6 +3,30 @@
 # should be the top-level directory (i.e. /workspace/your-app/) and then run:
 # rails runner code-along/0-classes.rb
 
+brians_favourite_food = "tacos"
+bens_favourite_food = "sushi"
+
+puts brians_favourite_food.upcase
+puts bens_favourite_food.upcase
+
 # String Class
 
 # String Methods
+class Dog
+
+    def speak
+        puts "woof"
+    end
+
+end
+
+class Cat
+
+    def speak
+        puts "Meow"
+    end
+end
+
+jenkins = Dog.new
+jenkins.speak
+
